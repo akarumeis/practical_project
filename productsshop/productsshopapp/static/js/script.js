5e9f9d078cf22dc77beeadfad81d30f0c158bbe3
MrYaroslav <xiaomiredist@gmail.com>
2023-05-02 22:48:02 +0300

    basket 0.1

--- productsshop/productsshopapp/static/js/script.js
+++ productsshop/productsshopapp/static/js/script.js
@@ -1,10 +1,46 @@
-$(document).ready(function() {
+// function minusCount() {
+//     console.log()
+//     // if (+count.innerHTML > 1){
+//     //     +count.innerHTML-- ;
+//     // }
+// }
+// function plusCount() {
+//     // +count.innerHTML++ ;
+// }
 
-    $('.send_btn').click(functions(){
-
-        $.ajax({
-
-            
-        })
-    })
-})
+$(".minus").click(function (e) {
+    e.preventDefault();
+    let value = $(this).parent().find("span[class=count]").html();
+    +value--
+    $(this).parent().find("span[class=count]").html(value);
+  });
+  
+  $(".plus").click(function (e) {
+    e.preventDefault();
+    let value = $(this).parent().find("span[class=count]").html();
+    +value++
+    $(this).parent().find("span[class=count]").html(value);
+  });
+  
+  // ------ Счётчик
+  
+  // ------
+  
+  $(document).ready(function () {
+    $(".send_btn").click(function (e) {
+      e.preventDefault();
+      const csrf = $("#form").find("input[name=csrfmiddlewaretoken]");
+      
+      $.ajax({
+        url: $(".url").val(),
+        type: "POST",
+        data: {
+          csrfmiddlewaretoken: csrf.val(),
+          id: $(this).val(),
+          count: $(".count").html(),
+        },
+        success: function () {},
+      });
+    });
+  });
+  
--- productsshop/productsshopapp/views.py
+++ productsshop/productsshopapp/views.py
@@ -1,5 +1,7 @@
+
 from django.shortcuts import render
 from .models import Product
+
 # Create your views here.
 
 def showShop(request):
@@ -8,10 +10,36 @@ def showShop(request):
         'products':  Product.objects.all(),
         'text_btn': 'Замовити',
     }
+
+    response = render(request, 'shop.html', context)
+
     if request.method == 'POST':
+
+        basket = request.COOKIES.get('cart', None)
         product_id = request.POST.get('id')
-        print(product_id)
-    return render(request, 'shop.html', context)
+        
+        count_basket_products = request.COOKIES.get('count', None)
+        get_count_product = request.POST.get('count')
+
+        print(get_count_product)
+
+        if basket != None:
+            basket_list = basket.split(' ')
+            count_basket_products_list =  count_basket_products.split(' ')
+
+            if product_id in basket_list:
+                #pass
+                index_product = basket_list.index(product_id)
+                count_basket_products_list[index_product] = get_count_product
+                response.set_cookie('count', ' '.join(count_basket_products_list))
+            else:
+                response.set_cookie('cart', basket+' '+product_id)
+                response.set_cookie('count', count_basket_products+' 1')
+        else:
+            response.set_cookie('cart', product_id)
+            response.set_cookie('count', get_count_product)
+            
+    return response
 
 
 
