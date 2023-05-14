d3ad7d33f135291e78adf9cfd84474a8cc530066
MrYaroslav <xiaomiredist@gmail.com>
2023-05-14 16:02:45 +0300

    the end

--- productsshop/productsshopapp/static/js/script.js
+++ productsshop/productsshopapp/static/js/script.js
@@ -1,45 +1,103 @@
-// function minusCount() {
-//     console.log()
-//     // if (+count.innerHTML > 1){
-//     //     +count.innerHTML-- ;
-//     // }
-// }
-// function plusCount() {
-//     // +count.innerHTML++ ;
-// }
-
-$(".minus").click(function (e) {
-  e.preventDefault();
-  let value = $(this).parent().find("span[class=amount]").html();
-  +value--
-  $(this).parent().find("span[class=amount]").html(value);
-});
-
-$(".plus").click(function (e) {
-  e.preventDefault();
-  let value = $(this).parent().find("span[class=amount]").html();
-  +value++
-  $(this).parent().find("span[class=amount]").html(value);
-});
-
-// ------ Счётчик
-
-// ------
-
 $(document).ready(function () {
-  $(".send_btn").click(function (e) {
+  const url = $(".url");
+  const csrf = $('#form input[name="csrfmiddlewaretoken"]');
+  const minusBtn = $(".minus");
+  const plusBtn = $(".plus");
+  const sendBtn = $(".send_btn");
+  const deleteBtn = $(".delete_btn");
+  const send_amount_btn = $(".send_amount");
+
+  // console.log($('#price').parent().parent().find('span.amount').html())
+  // console.log($('#price').parent().parent().find('span.amount').html())
+  // console.log($('.amount').parent().parent().find('input[name=price_one]').val())
+
+  $(".amount").each(function (index, element) {
+    let $amount = $(element);
+    let $product = $amount.closest(".product");
+    let priceOne = +$product.find("input[name=price_one]").val();
+    let totalPrice = priceOne * +$amount.text();
+    $product.find("span.price_span").html(totalPrice);
+  });
+
+  $(".price_span").each(function (index, element) {
+    $(".all_price").html(+$(".all_price").html() + +$(element).html());
+  });
+
+  minusBtn.click(function (e) {
     e.preventDefault();
-    const csrf = $("#form").find("input[name=csrfmiddlewaretoken]");
-    
+    let value = $(this).parent().find("span.amount").html();
+    if (+value > 1) {
+      --value;
+      $(this).parent().find("span.amount").html(value);
+    }
+  });
+
+  plusBtn.click(function (e) {
+    e.preventDefault();
+    let value = $(this).parent().find("span.amount").html();
+    ++value;
+    $(this).parent().find("span.amount").html(value);
+  });
+  sendBtn.click(function (e) {
+    e.preventDefault();
     $.ajax({
-      url: $(".url").val(),
+      url: url.val(),
       type: "POST",
       data: {
         csrfmiddlewaretoken: csrf.val(),
-        id: $(this).val(),
-        amount: $(this).parent().find("span[class=amount]").html(),
+        product: $(this).val(),
+        amount: $(this).parent().find("span.amount").html(),
       },
       success: function () {},
+    });
+  });
+
+  deleteBtn.click(function (e) {
+    e.preventDefault();
+    let idForm = $(`#${$(this).val()}`);
+    const csrf_del = idForm.find("input[name=csrfmiddlewaretoken]");
+    $.ajax({
+      url: url.val(),
+      type: "POST",
+      data: {
+        csrfmiddlewaretoken: csrf_del.val(),
+        id: $(this).val(),
+        button: "delete",
+      },
+      success: function () {
+        idForm.remove();
+        $(".all_price").html("0")
+        $(".price_span").each(function (index, element) {
+          $(".all_price").html(+$(".all_price").html() + +$(element).html());
+        });
+      },
+    });
+  });
+
+  send_amount_btn.click(function (e) {
+    e.preventDefault();
+    let idForm = $(`#${$(this).val()}`);
+    const csrf_amount = idForm.find("input[name=csrfmiddlewaretoken]");
+
+    $.ajax({
+      url: url.val(),
+      type: "POST",
+      data: {
+        csrfmiddlewaretoken: csrf_amount.val(),
+        id: $(this).val(),
+        amount: $(this).parent().find("span.amount").html(),
+        button: "change_amount",
+      },
+      success: function () {
+        const price_one = idForm.find("input[name=price_one]");
+        const amount = idForm.find("span.amount");
+        const price = idForm.find("span.price_span");
+        price.html(+price_one.val() * +amount.html());
+        $(".all_price").html("0")
+        $(".price_span").each(function (index, element) {
+          $(".all_price").html(+$(".all_price").html() + +$(element).html());
+        });
+      },
     });
   });
 });
--- productsshop/productsshopapp/views.py
+++ productsshop/productsshopapp/views.py
@@ -1,52 +1,84 @@
 
 from django.shortcuts import render
 from .models import Product
+from itertools import zip_longest
 import json
 
 # Create your views here.
 
+
 def showShop(request):
-    context = {'title': 'Магазин','text_btn': 'Замовити',}
-    product_list_for_html = [{'product': product, 'amount': 1} for product in Product.objects.all()]
+    context = {'title': 'Магазин', 'text_btn': 'Замовити', }
+    product_list_for_html = [{'product': product, 'amount': 1}
+                             for product in Product.objects.all()]
 
     if request.COOKIES.get('basket'):
         basket_value = json.loads(request.COOKIES.get('basket'))
         for check_product in product_list_for_html:
-            for check_product_basket in basket_value:
-                if check_product_basket['product']['id_product'] in  str(check_product['product'].pk):
-                    check_product['amount'] = check_product_basket['product']['amount']
+            for product_from_basket in basket_value:
+                if product_from_basket['product']['product_id'] in str(check_product['product'].pk):
+                    check_product['amount'] = product_from_basket['product']['amount']
     context['products'] = product_list_for_html
+
     response = render(request, 'shop.html', context)
 
-
     if request.method == 'POST':
-        get_product_id = request.POST.get('id')
-        get_product_amount = request.POST.get('amount')
-        basket_cookie = request.COOKIES.get('basket', None)
-        if basket_cookie: 
-            basket_cookie = json.loads(basket_cookie)
-            for product_from_basket in basket_cookie:
-                if get_product_id in product_from_basket['product']['id_product']:
-                    product_from_basket['product']['amount'] = get_product_amount
+        product_id = request.POST.get('product')
+        amount = request.POST.get('amount')
+        product_list = []
+
+        if request.COOKIES.get('basket'):
+            basket_value = json.loads(request.COOKIES.get('basket'))
+            for obj_from_basket in basket_value:
+                if product_id in obj_from_basket['product']['product_id']:
+                    obj_from_basket['product']['amount'] = amount
                     break
             else:
-                product_dict = {'product':{"id_product": get_product_id, "amount": get_product_amount}}
-                basket_cookie.append(product_dict)
+                product_dict = {'product': {
+                    'product_id': product_id, 'amount': amount, }}
+                basket_value.append(product_dict)
+            product_list = basket_value
         else:
-            basket_cookie = []
-            product_dict = {'product':{"id_product": get_product_id, "amount": get_product_amount}}
-            basket_cookie.append(product_dict)
+            product_dict = {'product': {
+                'product_id': product_id, 'amount': amount, }}
+            product_list.append(product_dict)
+        response.set_cookie('basket', json.dumps(product_list))
 
-        response.set_cookie('basket', json.dumps(basket_cookie))
     return response
-
 
 
 def showBasket(request):
     context = {
-        'title': 'Кошик'
+        'title': 'Кошик',
+        'text_btn': 'Delete'
     }
-    return render(request, 'basket.html', context)
 
+    if request.COOKIES.get('basket'):
+        basket_cookie = json.loads(request.COOKIES.get('basket', None))
+        for change_product in basket_cookie:
+            change_product['product']['product_id'] = Product.objects.get(
+                pk=change_product['product']['product_id'])
+        context['products'] = basket_cookie
 
+    response = render(request, 'basket.html', context)
 
+    if request.method == 'POST':
+        if request.COOKIES.get('basket'):
+
+            get_product_id = request.POST.get('id')
+            basket_cookie = json.loads(request.COOKIES.get('basket', None))
+
+            if request.POST.get('button') == 'delete':
+                for change_product in basket_cookie:
+                    if get_product_id in change_product['product']['product_id']:
+                        basket_cookie.pop(basket_cookie.index(change_product))
+
+            elif request.POST.get('button') == 'change_amount':
+                get_product_amount = request.POST.get('amount')
+                for change_product in basket_cookie:
+                    if get_product_id in change_product['product']['product_id']:
+                        change_product['product']['amount'] = get_product_amount
+
+            response.set_cookie('basket', json.dumps(basket_cookie))
+
+    return response
